a87c667bf4491935750bea0122492e3055ed902c
jose <baez91876@gmail.com>
2026-09-09 11:20:13 -0500

    Remove project comments

--- hoy/src/router/routes.js
+++ hoy/src/router/routes.js
@@ -12,8 +12,6 @@ const routes = [
     component: () => import('@/pages/NosotrosPage.vue')
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('@/pages/ErrorNotFound.vue'),
